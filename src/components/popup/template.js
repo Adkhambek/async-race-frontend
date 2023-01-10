8d9ee1f3a8895ccb5bcde593d3d1aa59933c083c
Adham <muzaffarov.adham@gmail.com>
2023-01-10 22:08:49 +0500

    feat: add images

--- src/components/popup/template.js
+++
@@ -1,6 +0,0 @@
-function template(winner) {
-  return `<img src="./assets/trophy.png" alt="trophy" />
-  <h2>The WINNER is<br /><span class="name">${winner.name}</span><br />${winner.time} sec</h2>`;
-}
-
-export default template;
